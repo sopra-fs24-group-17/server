ae4d00b5cf4f0c3b8add30e8733e3ca81b937356
Liam Kane <liam.kane@uzh.ch>
2024-04-08 19:02:57 +0200

    added ImageService docstrings

--- src/main/java/ch/uzh/ifi/hase/soprafs24/service/ImageService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/service/ImageService.java
@@ -12,7 +12,13 @@ import java.util.UUID;
 @Service
 public class ImageService {
 
-    // Save image in a local directory
+    /**
+     * Saves an image file to a specified local directory and names it uniquely to avoid overwrites.
+     * @param uploadDirectory the directory path where the image should be saved.
+     * @param imageFile the multipart file uploaded by the user, to be saved on disk.
+     * @return a String containing the unique file name under which the image was saved.
+     * @throws IOException if an I/O error occurs during file saving.
+     */
     public String saveImageToStorage(String uploadDirectory, MultipartFile imageFile) throws IOException {
         String uniqueFileName = UUID.randomUUID().toString() + "_" + imageFile.getOriginalFilename();
 
@@ -28,7 +34,13 @@ public class ImageService {
         return uniqueFileName;
     }
 
-    // To view an image
+    /**
+     * Retrieves an image as a byte array from the specified directory.
+     * @param imageDirectory the directory from which to retrieve the image.
+     * @param imageName the name of the image file to retrieve.
+     * @return a byte array representing the image, or null if the image does not exist.
+     * @throws IOException if an I/O error occurs during file reading.
+     */
     public byte[] getImage(String imageDirectory, String imageName) throws IOException {
         Path imagePath = Path.of(imageDirectory, imageName);
 
@@ -40,7 +52,13 @@ public class ImageService {
         }
     }
 
-    // Delete an image
+    /**
+     * Deletes an image file from the specified directory.
+     * @param imageDirectory the directory from which the image should be deleted.
+     * @param imageName the name of the image file to delete.
+     * @return a String indicating the outcome of the delete operation ("Success" or "Failed").
+     * @throws IOException if an I/O error occurs during file deletion.
+     */
     public String deleteImage(String imageDirectory, String imageName) throws IOException {
         Path imagePath = Path.of(imageDirectory, imageName);
 
