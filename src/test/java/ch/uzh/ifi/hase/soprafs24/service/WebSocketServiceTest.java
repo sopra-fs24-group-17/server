feb07169141b1c7b997daffc7689346b5ee24936
Kevin Bründler <kevinbrundler@Kevins-MBP.yallo.box>
2024-04-13 19:56:47 +0200

    Enhanced login and logout events for websocket

--- src/test/java/ch/uzh/ifi/hase/soprafs24/service/WebSocketServiceTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/service/WebSocketServiceTest.java
@@ -44,7 +44,7 @@ public class WebSocketServiceTest {
         String userName = "testUser";
         Long userId = 456L;
         webSocketService.sendMessageFriendLogin(userName, userId);
-        verify(messagingTemplate).convertAndSend("/login/" + userId, userName);
+        verify(messagingTemplate).convertAndSend("/login" , userName);
     }
 
     @Test
@@ -52,7 +52,7 @@ public class WebSocketServiceTest {
         String userName = "testUser";
         Long userId = 789L;
         webSocketService.sendMessageFriendLogout(userName, userId);
-        verify(messagingTemplate).convertAndSend("/logout/" + userId, userName);
+        verify(messagingTemplate).convertAndSend("/logout", userName);
     }
 
     @Test
@@ -85,6 +85,13 @@ public class WebSocketServiceTest {
         Long gameId = 234L;
         webSocketService.sendMessageLeftUser(userName, gameId);
         verify(messagingTemplate).convertAndSend("/game/" + gameId, userName);
+    }
+
+    @Test
+    public void sendMessageGameCreatedTest() {
+        Long gameId = 1234L;
+        webSocketService.sendMessageGameCreated(gameId);
+        verify(messagingTemplate).convertAndSend("/game/new", gameId);
     }
 
 }
