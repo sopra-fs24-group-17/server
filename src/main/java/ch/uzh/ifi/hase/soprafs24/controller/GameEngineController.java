2138b289446eab750b2630db877b8d0528454a90
Kevin Bründler <kevinbrundler@Kevins-MBP.yallo.box>
2024-04-17 15:57:20 +0200

    Closes issue #100

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/GameEngineController.java
@@ -0,0 +1,2 @@
+package ch.uzh.ifi.hase.soprafs24.controller;public class GameEngineController {
+}
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/websocket/dto/CardMoveRequest.java
@@ -0,0 +1,2 @@
+package ch.uzh.ifi.hase.soprafs24.websocket.dto;public class CardMoveRequest {
+}
