05e385c236da0564961d579fee83149007327a5a
Kevin Bründler <kevinbrundler@Kevins-MBP.yallo.box>
2024-04-17 13:46:42 +0200

    Closes issue #198

--- src/main/java/ch/uzh/ifi/hase/soprafs24/eventlistener/GameEngineEventListener.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/eventlistener/GameEngineEventListener.java
@@ -1,9 +1,6 @@
 package ch.uzh.ifi.hase.soprafs24.eventlistener;
 
-import ch.uzh.ifi.hase.soprafs24.event.DrawCardsEvent;
-import ch.uzh.ifi.hase.soprafs24.event.GameCreationEvent;
-import ch.uzh.ifi.hase.soprafs24.event.PeekIntoDeckEvent;
-import ch.uzh.ifi.hase.soprafs24.event.ShufflingEvent;
+import ch.uzh.ifi.hase.soprafs24.event.*;
 import ch.uzh.ifi.hase.soprafs24.service.WebSocketService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,6 +36,12 @@ public class GameEngineEventListener {
     public void onPeekIntoDeck(PeekIntoDeckEvent event) {
         logger.info("PeekIntoDeckEvent triggered for game ID: {} by user {}", event.getGameId(), event.getInvokingPlayerUserName());
         webSocketService.sendMessagePeekIntoDeck(event.getGameId(), event.getInvokingPlayerUserName());
+    }
+
+    @EventListener
+    public void returnExplosionToDeck(ExplosionReturnedToDeckEvent event) {
+        logger.info("ExplosionReturnedToDeckEvent triggered for game ID: {} by user {}", event.getGameId(), event.getInvokingPlayerUserName());
+        webSocketService.sendMessageExplosionReturnedToDeck(event.getGameId(), event.getInvokingPlayerUserName());
     }
 
 }
